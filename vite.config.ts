11e4042b7461f9a2c2a7b6591b304a58ef03fba0
SHUBHAM <waghmareshubham52@gmail.com>
2025-02-08 13:29:31 +0530

    Enabled code-splitting for better performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Website/',
+  base: '/Official/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    chunkSizeWarningLimit: 1024, // Adjust warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Move all npm packages into a separate "vendor" chunk
+          }
+          if (id.includes('physics')) {
+            return 'physics'; // Create a separate chunk for physics files
+          }
+          if (id.includes('Hero3D')) {
+            return 'hero'; // Separate 3D-related code
+          }
+        },
+      },
+    },
   },
 });
